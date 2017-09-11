1d28183c73b7848a1c305bb7233ebc493ca79b6e
Joshua Frattarola <me@joshuafrattarola.com>
2017-09-11 15:35:24 -0700

    latest

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D
 import argparse
 
 slash='\\'
@@ -53,10 +53,17 @@ if __name__ == '__main__':
 	X_train, y_train = load_data(args.data_path)
 	
 	model = Sequential()
-	model.add(Flatten(input_shape=(160,320,3)))
+	model.add(Lambda(lambda x: x/255.0 -0.5, input_shape=(160,320,3)))
+	model.add(Convolution2D(6,5,5, activation='relu'))
+	model.add(MaxPooling2D())
+	model.add(Convolution2D(6,5,5,activation='relu'))
+	model.add(MaxPooling2D())
+	model.add(Flatten())
+	model.add(Dense(120))
+	model.add(Dense(84))
 	model.add(Dense(1))
 
 	model.compile(loss='mse', optimizer='adam')
-	model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
+	model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 	model.save('model.h5')
