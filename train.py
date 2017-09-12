a423f324808fc80d9edec9f950e586443d476bd6
Joshua Frattarola <me@joshuafrattarola.com>
2017-09-11 20:13:12 -0700

    latest

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from keras.models import Sequential, Model
 from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D, Cropping2D
+from keras.optimizers import Adam
 import argparse
 import matplotlib.pyplot as plt
 import os
@@ -13,6 +14,19 @@ import random
 slash='\\'
 steering_correction = 0.2
 nb_epoch=20
+
+def rgb2gray(rgb):
+	if len(rgb.shape)==4:
+		G = np.zeros([len(rgb), rgb.shape[1], rgb.shape[2], 1])
+		for i, img in enumerate(rgb):
+			r,g,b = img[:,:,0], img[:,:,1], img[:,:,2]
+			gray =  (0.2989*r + 0.5870*g + 0.1140*b)
+			G[i] = gray.reshape(rgb.shape[1], rgb.shape[2], 1)
+			return G
+	else:
+		r, g, b = rgb[:,:0], rgb[:,:,1], rgb[:,:,2]
+		gray = (0.2989*r + 0.5870*g + 0.1140*b)
+		return gray
 
 def generator(data_path, samples, batch_size=32):
 	if len(data_path) > 0 and data_path[-1] != '\\' and data_path[-1] != '/':
@@ -36,19 +50,21 @@ def generator(data_path, samples, batch_size=32):
 				left_angle = center_angle + steering_correction
 				right_angle = center_angle - steering_correction
 				if center_image is not None and left_image is not None and right_image is not None:
-					images.append(center_image)
-					images.append(left_image)
-					images.append(right_image)
-					angles.append(center_angle)
-					angles.append(left_angle)
-					angles.append(right_angle)
-					#flip imagesage to increase data variability
-					images.append(cv2.flip(center_image, 1))
-					images.append(cv2.flip(left_image, 1))
-					images.append(cv2.flip(right_image, 1))
-					angles.append(center_angle * (-1))
-					angles.append(left_angle * (-1))
-					angles.append(right_angle * (-1))
+					if np.random.randint(2) == 1:
+						images.append(center_image)
+						#images.append(left_image)
+						#images.append(right_image)
+						angles.append(center_angle)
+						#angles.append(left_angle)
+						#angles.append(right_angle)
+					else :
+						#flip imagesage to increase data variability
+						images.append(cv2.flip(center_image, 1))
+						#images.append(cv2.flip(left_image, 1))
+						#images.append(cv2.flip(right_image, 1))
+						angles.append(center_angle * (-1))
+						#angles.append(left_angle * (-1))
+						#angles.append(right_angle * (-1))
 				else:
 					print('Error: could not load {}'.format(curr))
 			X_train = np.array(images)
@@ -75,7 +91,7 @@ if __name__ == '__main__':
 		type=str,
 		help='Path to training data and images.',
 		nargs='?',
-		default=''
+		default='data'
 	)
 	args = parser.parse_args()
 
@@ -83,12 +99,12 @@ if __name__ == '__main__':
 	train_generator = generator(args.data_path, train_samples, batch_size=32)
 	validation_generator = generator(args.data_path, validation_samples, batch_size=32)
 
-#	ch, row, col = 3, 80, 320 #Trimmed image format
+	ch, row, col = 3, 160, 320 #Trimmed image format
 
 	model = Sequential()
 
 	model.add(Lambda(lambda x: x/255. -0.5, 
-		input_shape=(160,320,3)))
+		input_shape=(row,col,ch)))
 	model.add(Cropping2D(cropping=((70,25),(0,0))))
 	model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
 	#model.add(MaxPooling2D())
@@ -104,7 +120,7 @@ if __name__ == '__main__':
 	model.add(Dense(10, activation='relu'))
 	model.add(Dense(1))
 
-	model.compile(loss='mse', optimizer='adam')
+	model.compile(loss='mse', optimizer=Adam(lr=1e-4))
 
 	#histogram
 	history_object = model.fit_generator( train_generator, 
